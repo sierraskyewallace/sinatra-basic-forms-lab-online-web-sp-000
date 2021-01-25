ae11b3408835864f495151b55be5d2b49391bd35
Sierra Wallace <sskwallace@gmail.com>
2021-01-25 01:40:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,5 +7,7 @@ class App < Sinatra::Base
       get '/new' do 
         erb :create_puppy
       end
-      
+      post '/puppy' do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        erb :display_puppy
 end
